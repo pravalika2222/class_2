97f5b0da02470d6a4909eeda9d86cf918e32bc33
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-06-24 22:10:26 +0530

    Add files via upload

---
+++ cancatenation.py
@@ -0,0 +1,25 @@
+# 'canatenation is joining or combining two or more multiples strings together'
+# x='hi'
+# y='hey'
+# print(x+y) # hihey    # there is no space will occur in o/p
+# print(y+x) # heyhi    # here  + is cancatenation syntax 
+
+# a='hi'
+# b='hey'
+# print(''+'hi')  # hi             # here there is empty string is there('')
+# print(' '+'hi') #  hi         # here there in string the space is their
+
+# 'isspace are which definds as true or false '
+# 'true : there is single space or multiple space is there'
+# 'false : there is empty string or something inside the string'
+
+# ex for isspce ;
+# print(''.isspace())     # false  ( bcoz ,there is empty string)
+# print(','.isspace())    # false  ( bcoz , there is something is there in string)
+# print('hey'.isspace())  # false  ( bcoz, there  is something is there in string)
+# print(' '.isspace())    # true    ( bcoz , there is single space in the string)
+# print('    '.isspace()) # true    ( bcoz , there is multiple space in the string)
+
+# print('hi'+''+'hello')  # hihello      # cancatenation 
+# print('hi'+' '+'hello') # hi hello     # cancatenation
+# print('hi'+'_'+'hello') # hi_hello     # cancatenation
